6db79ae8c5073055e1d2cf0a4c3daf9016aadb0d
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2024-01-18 19:20:17 +0800

    feat: Implement input validation for numbers, decimal, and operators

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,65 @@ const App = () => {
   const [formula, setFormula] = useState('');
 
   const handleNumber = (number) => {
-    if (!$('#formula').text()) {
+    if (!$('#formula').text() || $('#formula').text() === '0') {
       setInput(`${number}`);
       setFormula(`${number}`);
+    } else if ( /[-+*/]$/.test(formula) ) {
+      setInput(`${number}`);
+      setFormula(`${formula}${number}`);
+    } else if ( /[-+*/]0$/.test(formula) ) {
+      setInput(`${number}`);
+      setFormula(`${formula.slice(0, formula.length - 1)}${number}`);
     } else {
       setInput(`${input}${number}`);
-      setFormula(`${input}${number}`);
+      setFormula(`${formula}${number}`);
     }
   };
+
+  const handleDecimal = () => {
+    if (input.indexOf('.') === -1 && /\d/.test(input)) {
+      setInput(`${input}.`);
+      !$('#formula').text() 
+        ? setFormula('0.') 
+        : setFormula(`${formula}.`);
+    } 
+    if ( /[-+*/]/.test(input) ) {
+      setInput('0.');
+      setFormula(`${formula}0.`);
+    }
+  }
+
+  const handleOperator = (operator) => {
+    if (operator === 'x') {
+      operator = '*';
+    }
+
+    if (!$('#formula').text()) {
+      setInput(`${operator}`);
+      operator === '-' 
+        ? setFormula(`${operator}`) // negative number
+        : setFormula(`${input}${operator}`); // 0 followed by operator
+    }
+    
+    if ( /[^-+*/]$/.test(formula) ) { // last char is number/decimal
+      setInput(`${operator}`);
+      /[.]$/.test(formula) 
+        ? setFormula(`${formula}0${operator}`) 
+        : setFormula(`${formula}${operator}`);
+    } else { // last char is operator
+      setInput(`${operator}`);
+      if ( /\d[-+*/]$/.test(formula) ) {
+          operator !== '-' 
+            ? setFormula(`${formula.slice(0, formula.length - 1)}${operator}`)
+            : setFormula(`${formula}${operator}`);
+      }
+      if ( /[-+*/][-+*/]$/.test(formula) && operator !== '-' ) {
+          setFormula(`${formula.slice(0, formula.length - 2)}${operator}`); 
+      }
+    }
+  }
+
+  // TODO: handleEquals function
 
   const handleClear = () => {
     setInput('0');
@@ -42,10 +93,11 @@ const App = () => {
           id={operator.id} 
           className='operator'
           type="button"
+          onClick={() => handleOperator(operator.text)}
         >{operator.text}</button>
       ))}
 
-      <button id="decimal" type="button">.</button>
+      <button id="decimal" type="button" onClick={handleDecimal}>.</button>
       <button id="clear" type="button" onClick={handleClear}>AC</button>
     </div>
   )
