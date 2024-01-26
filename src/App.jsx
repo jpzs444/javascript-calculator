c0ea42867d795ea37abe2b0ac3ef2a0e38bebf75
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2024-01-26 12:18:18 +0800

    style: Implement background design

--- src/App.jsx
+++ src/App.jsx
@@ -93,38 +93,51 @@ const App = () => {
 
   return (
     <main className="container">
-      <div className="calculator">
-        <div className="output">
-          <div id="formula">{formula}</div>
-          <div id="display">{input}</div>
-        </div>
-        
-        <div className="input">
-          <button id="clear" type="button" onClick={handleClear}>AC</button>
+      <div className="positioning">
+        <div className="calculator">
+          <div className="output">
+            <div id="formula">{formula}</div>
+            <div id="display">{input}</div>
+          </div>
           
-          {numbers.map((number) => (
-            <button 
-              key={number.id} 
-              id={number.id} 
-              className="number"
-              type="button"
-              onClick={() => handleNumber(number.text)}
-            >{number.text}</button>
-          ))}
+          <div className="input">
+            <button id="clear" type="button" onClick={handleClear}>AC</button>
+            
+            {numbers.map((number) => (
+              <button 
+                key={number.id} 
+                id={number.id} 
+                className="number"
+                type="button"
+                onClick={() => handleNumber(number.text)}
+              >{number.text}</button>
+            ))}
 
-          {operators.map((operator) => (
-            <button 
-              key={operator.id}
-              id={operator.id} 
-              className='operator'
-              type="button"
-              onClick={() => handleOperator(operator.text)}
-            >{operator.text}</button>
-          ))}
+            {operators.map((operator) => (
+              <button 
+                key={operator.id}
+                id={operator.id} 
+                className='operator'
+                type="button"
+                onClick={() => handleOperator(operator.text)}
+              >{operator.text}</button>
+            ))}
 
-          <button id="decimal" type="button" onClick={handleDecimal}>.</button>
-          <button id="equals" type="button" onClick={handleEquals}>=</button>
+            <button id="decimal" type="button" onClick={handleDecimal}>.</button>
+            <button id="equals" type="button" onClick={handleEquals}>=</button>
+          </div>
+          
+          {/* For bg */}
+          <div className="circle-glow circle-glow-1"></div>
+          <div className="circle-glow circle-glow-2"></div>
         </div>
+
+        {/* For bg */}
+        <div className="circle-bg"></div>
+        <div className="rectangle-shadow"></div>
+        <div className="dot dot-1"></div>
+        <div className="dot dot-2"></div>
+        <div className="square" title="Design based on Sadek Hossen Rony's Calculator App UI Design (Figma Community)">?</div>
       </div>
     </main>
   )
